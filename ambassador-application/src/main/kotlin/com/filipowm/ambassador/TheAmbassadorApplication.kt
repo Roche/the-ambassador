5d51e1d85d5442c195f857b3e10a698e71946d22
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2021-09-20 00:33:56 +0200

    disable setting up unneeded deafault user

--- ambassador-application/src/main/kotlin/com/filipowm/ambassador/TheAmbassadorApplication.kt
+++ ambassador-application/src/main/kotlin/com/filipowm/ambassador/TheAmbassadorApplication.kt
@@ -1,10 +1,11 @@
 package com.filipowm.ambassador
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [ReactiveUserDetailsServiceAutoConfiguration::class])
 @EnableConfigurationProperties
 open class TheAmbassadorApplication
 
