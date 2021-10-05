aff73de221527aa4bf9e7f999853fec8e438812b
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2021-10-05 08:21:44 +0200

    disable Lingua temporarily, cause it is anyway yet not used and to minimize resulting jar size

--- ambassador-document-analyzer/build.gradle.kts
+++ ambassador-document-analyzer/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.slf4j:slf4j-api:1.7.31")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
-    implementation("com.github.pemistahl:lingua:1.0.3")
+//    implementation("com.github.pemistahl:lingua:1.0.3")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
--- ambassador-document-analyzer/src/main/kotlin/com/filipowm/ambassador/document/LanguageDetector.kt
+++ ambassador-document-analyzer/src/main/kotlin/com/filipowm/ambassador/document/LanguageDetector.kt
@@ -1,7 +1,7 @@
 package com.filipowm.ambassador.document
 
-import com.github.pemistahl.lingua.api.LanguageDetectorBuilder
-import com.github.pemistahl.lingua.api.Language as LinguaLanguage
+//import com.github.pemistahl.lingua.api.LanguageDetectorBuilder
+//import com.github.pemistahl.lingua.api.Language as LinguaLanguage
 
 interface LanguageDetector {
 
@@ -11,22 +11,35 @@ interface LanguageDetector {
     companion object {
         fun default(): LanguageDetector {
             // disable not popular or unlikely languages to minimize memory impact
-            val lingua = LanguageDetectorBuilder
-                .fromLanguages(LinguaLanguage.ENGLISH, LinguaLanguage.FRENCH, LinguaLanguage.GERMAN)
-//                .fromAllLanguagesWithout(
-//                    LinguaLanguage.ESPERANTO,
-//                    LinguaLanguage.LATIN,
-//                    LinguaLanguage.BASQUE,
-//                    LinguaLanguage.GANDA,
-//                    LinguaLanguage.SOTHO,
-//                    LinguaLanguage.TSONGA,
-//                    LinguaLanguage.TSWANA,
-//                    LinguaLanguage.XHOSA,
-//                    LinguaLanguage.BASQUE
-//                )
-                .withMinimumRelativeDistance(0.1)
-                .build()
-            return LinguaLanguageDetector(lingua)
+//            val lingua = LanguageDetectorBuilder
+//                .fromLanguages(LinguaLanguage.ENGLISH, LinguaLanguage.FRENCH, LinguaLanguage.GERMAN)
+////                .fromAllLanguagesWithout(
+////                    LinguaLanguage.ESPERANTO,
+////                    LinguaLanguage.LATIN,
+////                    LinguaLanguage.BASQUE,
+////                    LinguaLanguage.GANDA,
+////                    LinguaLanguage.SOTHO,
+////                    LinguaLanguage.TSONGA,
+////                    LinguaLanguage.TSWANA,
+////                    LinguaLanguage.XHOSA,
+////                    LinguaLanguage.BASQUE
+////                )
+//                .withMinimumRelativeDistance(0.1)
+//                .build()
+//            return LinguaLanguageDetector(lingua)
+            return FixedLanguageDetector(Language.ENGLISH)
         }
     }
+}
+
+internal class FixedLanguageDetector(val language: Language) : LanguageDetector {
+    override fun warmUp() {
+    }
+
+    override fun detectLanguageOf(text: String): Language = if (text.isBlank()) {
+        Language.UNKNOWN
+    } else {
+        language
+    }
+
 }
