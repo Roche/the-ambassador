172b1ff901a23ee57b6064f3d2c4c0bc15d0a9b2
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2021-10-17 00:04:51 +0200

    feat(project): timeline feature does not provide raw data, but statistical information about the timeline

--- ambassador-model/src/main/kotlin/com/filipowm/ambassador/model/feature/TimelineFeature.kt
+++ ambassador-model/src/main/kotlin/com/filipowm/ambassador/model/feature/TimelineFeature.kt
@@ -1,5 +1,26 @@
 package com.filipowm.ambassador.model.feature
 
+import com.filipowm.ambassador.extensions.round
+import com.filipowm.ambassador.model.IndexEntry
 import com.filipowm.ambassador.model.stats.Timeline
 
-open class TimelineFeature(value: Timeline? = Timeline(), name: String) : AbstractFeature<Timeline>(value, name)
+open class TimelineFeature(value: Timeline? = Timeline(), name: String) : AbstractFeature<Timeline>(value, name) {
+    override fun asIndexEntry(): IndexEntry {
+        val timeline = value.orElse { Timeline() }
+        if (timeline.empty()) {
+            return IndexEntry.no()
+        }
+        val series = timeline.series()
+        return IndexEntry.of(
+            name, mapOf(
+                "aggregated" to timeline.aggregation.name,
+                "periods" to timeline.count(),
+                "sum" to timeline.sum(),
+                "mean" to timeline.average().round(2),
+                "startDate" to series.first().date,
+                "endDate" to series.last().date
+            )
+        )
+    }
+
+}
