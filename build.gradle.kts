040306732de964e655c80e92323ca37034530eec
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2021-11-06 05:34:04 +0100

    chore: create task tot merge Detekt reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.report.ReportMergeTask
+
 plugins {
     idea
     `maven-publish`
@@ -6,4 +8,30 @@ plugins {
 idea {
     module.isDownloadJavadoc = true
     module.isDownloadSources = true
+}
+
+fun createMergeTask(outputName: String): RegisteringDomainObjectDelegateProviderWithTypeAndAction<out TaskContainer, ReportMergeTask> {
+    return tasks.registering(ReportMergeTask::class) {
+        output.set(rootProject.buildDir.resolve("reports/detekt/$outputName"))
+    }
+}
+
+val reportMergeSarif by createMergeTask("merge.sarif")
+val reportMergeXml by createMergeTask("merge.xml")
+
+subprojects {
+    plugins.withType(io.gitlab.arturbosch.detekt.DetektPlugin::class) {
+        tasks.withType(io.gitlab.arturbosch.detekt.Detekt::class) detekt@{
+            finalizedBy(reportMergeSarif)
+            finalizedBy(reportMergeXml)
+
+            reportMergeSarif.configure {
+                input.from(this@detekt.sarifReportFile)
+            }
+
+            reportMergeXml.configure {
+                input.from(this@detekt.xmlReportFile)
+            }
+        }
+    }
 }
