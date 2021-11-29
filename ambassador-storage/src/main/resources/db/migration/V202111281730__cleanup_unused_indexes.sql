cfc8d7d05d1babc5a40dd30bfc3ba5b95cd23471
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2021-11-29 12:29:23 +0100

    fix: creation of indexes together with dropping causing flyway to fail

---
+++ ambassador-storage/src/main/resources/db/migration/V202111281730__cleanup_unused_indexes.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS visibility_idx;
+DROP INDEX IF EXISTS project_id_idx;
--- ambassador-storage/src/main/resources/db/migration/V202111281731__projects_search_index.sql
+++ ambassador-storage/src/main/resources/db/migration/V202111281731__projects_search_index.sql
@@ -1,4 +1,1 @@
-DROP INDEX IF EXISTS visibility_idx;
 CREATE INDEX CONCURRENTLY project_visibility_tags_idx ON project((project ->> 'visibility'), (project -> 'tags'));
-
-DROP INDEX IF EXISTS project_id_idx;
