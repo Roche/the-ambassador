87e0c5926aa0001baafef9fbe5233dd20551ffa0
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2021-12-17 16:30:05 +0100

    chore: temporarily disable CORS until made configurable

--- ambassador-application/src/main/kotlin/com/roche/ambassador/configuration/web/WebConfiguration.kt
+++ ambassador-application/src/main/kotlin/com/roche/ambassador/configuration/web/WebConfiguration.kt
@@ -3,6 +3,7 @@ package com.roche.ambassador.configuration.web
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver
 import org.springframework.format.FormatterRegistry
+import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer
 
@@ -16,4 +17,13 @@ internal open class WebConfiguration : WebFluxConfigurer {
     override fun addFormatters(registry: FormatterRegistry) {
         registry.addConverterFactory(StringToEnumConverter)
     }
+
+    // FIXME disable CORS until made configurable
+    override fun addCorsMappings(corsRegistry: CorsRegistry) {
+        corsRegistry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .maxAge(3600)
+    }
+
 }
