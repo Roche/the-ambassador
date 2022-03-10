8a3c63e9574d5a2a6b2c9f1f7cdfb379e886a826
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2022-03-10 08:18:11 +0100

    feat: handle default web errors

---
+++ ambassador-application/src/main/kotlin/com/roche/ambassador/configuration/web/GlobalErrorWebExceptionHandler.kt
@@ -0,0 +1,45 @@
+package com.roche.ambassador.configuration.web
+
+import com.roche.ambassador.commons.api.Message
+import com.roche.ambassador.configuration.i18n.ReactiveMessageSource
+import kotlinx.coroutines.runBlocking
+import org.springframework.boot.autoconfigure.web.WebProperties
+import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler
+import org.springframework.boot.web.error.ErrorAttributeOptions
+import org.springframework.boot.web.reactive.error.ErrorAttributes
+import org.springframework.context.ApplicationContext
+import org.springframework.http.codec.ServerCodecConfigurer
+import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.server.*
+import reactor.core.publisher.Mono
+
+internal class GlobalErrorWebExceptionHandler(
+    private val messageSource: ReactiveMessageSource, serverCodecConfigurer: ServerCodecConfigurer,
+    errorAttributes: ErrorAttributes, resources: WebProperties, applicationContext: ApplicationContext
+) : AbstractErrorWebExceptionHandler(errorAttributes, resources.resources, applicationContext) {
+
+    init {
+        setMessageWriters(serverCodecConfigurer.writers)
+        setMessageReaders(serverCodecConfigurer.readers)
+    }
+
+    override fun getRoutingFunction(
+        errorAttributes: ErrorAttributes
+    ): RouterFunction<ServerResponse> {
+        return RouterFunctions.route(RequestPredicates.all()) {
+            runBlocking {
+                renderErrorResponse(it)
+            }
+        }
+    }
+
+    private suspend fun renderErrorResponse(
+        request: ServerRequest
+    ): Mono<ServerResponse> {
+        val errorPropertiesMap = getErrorAttributes(request, ErrorAttributeOptions.defaults())
+        val status = errorPropertiesMap["status"] as Int
+        val msg = messageSource.getMessage("web.errors.$status", "Request processing has failed")
+        val me = Message(msg)
+        return ServerResponse.status(status).json().body(BodyInserters.fromValue(me))
+    }
+}
--- ambassador-application/src/main/kotlin/com/roche/ambassador/configuration/web/WebConfiguration.kt
+++ ambassador-application/src/main/kotlin/com/roche/ambassador/configuration/web/WebConfiguration.kt
@@ -1,18 +1,34 @@
 package com.roche.ambassador.configuration.web
 
+import com.roche.ambassador.configuration.i18n.ReactiveMessageSource
+import org.springframework.boot.autoconfigure.web.WebProperties
+import org.springframework.boot.web.reactive.error.ErrorAttributes
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver
 import org.springframework.format.FormatterRegistry
 import org.springframework.format.datetime.DateFormatter
 import org.springframework.format.datetime.DateFormatterRegistrar
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar
+import org.springframework.http.codec.ServerCodecConfigurer
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer
-import java.time.format.DateTimeFormatter
 
 @Configuration
 internal class WebConfiguration : WebFluxConfigurer {
+
+    @Bean
+    @Order(-1)
+    fun errorWebExceptionHandler(
+        reactiveMessageSource: ReactiveMessageSource,
+        errorAttributes: ErrorAttributes, webProperties: WebProperties, serverCodecConfigurer: ServerCodecConfigurer, applicationContext: ApplicationContext
+    ): ErrorWebExceptionHandler {
+        return GlobalErrorWebExceptionHandler(reactiveMessageSource, serverCodecConfigurer, errorAttributes, webProperties, applicationContext)
+    }
 
     override fun configureArgumentResolvers(configurer: ArgumentResolverConfigurer) {
         configurer.addCustomResolver(ReactivePageableHandlerMethodArgumentResolver())
@@ -38,9 +54,6 @@ internal class WebConfiguration : WebFluxConfigurer {
 
     // FIXME disable CORS until made configurable
     override fun addCorsMappings(corsRegistry: CorsRegistry) {
-        corsRegistry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("*")
-            .maxAge(3600)
+        corsRegistry.addMapping("/**").allowedOrigins("*").allowedMethods("*").maxAge(3600)
     }
 }
